5dcc19eaac13a2ab2ab2ce635cca05a0ec9128df
Duisterhof <bduister@andrew.cmu.edu>
2023-09-08 15:50:33 -0400

    progress on automagical evaluation

--- scripts/eval_depth.py
+++ scripts/eval_depth.py
@@ -6,68 +6,113 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 
 import pyroexr
+import glob
 
 # some data (512x512)
 
 # map the normalized data to colors
 # image is now RGBA (512x512x4) 
 
-def load_exr(path):
-    exr = pyroexr.load(path)
-    shape = exr.channels()['B'].shape
-    img = np.zeros((shape[0],shape[1],3))
-    img[:,:,0] = exr.channels()['R']
-    img[:,:,1] = exr.channels()['G']
-    img[:,:,2] = exr.channels()['B']
-
-    return img
-
-
-
-to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--infer', type=str, default='depth.npy', help='path to depth file')
-parser.add_argument('--gt', type=str, default='gt.npy', help='path to depth file')
+parser.add_argument('--dir',type=str,default='depth_evaluation')
 parser.add_argument('--scale',type=float,default=0.33)
 parser.add_argument('--debug',action='store_true')
 parser.add_argument('--thresh',type=float,default=1.5)
 args = parser.parse_args()
 
-# gt_depth = load_exr(args.gt)
-# N x H x W
-gt_depth = np.load(args.gt)[:,:,:,0]
-# gt_depth = gt_depth[:,:,:,0]
-# N x H x W
-infer_depth = np.load(args.infer)
-infer_depth *= (1.0/args.scale)
-if args.debug:
-    ax1 = plt.subplot(1,2,1)
-    ax1.imshow(gt_depth[0])
-    ax2 = plt.subplot(1,2,2)
-    ax2.imshow(infer_depth[0])
+class Depth:
+    def __init__(self,depth,gt):
+        self.depth = depth
+        self.gt = gt
 
-    #title first plot 
-    ax1.set_title('Ground Truth')
-    ax2.set_title('Inferred Depth')
+class Scene:
+    # init
+    def __init__(self,scene_dir):
+        self.depths = []
+        self.scene_dir = scene_dir
+        self.depths_files = glob.glob(os.path.join(scene_dir,'*.npy'))
+        self.depths_files = [f for f in self.depths_files if 'gt' not in f]
+        self.gt_depth_file = os.path.join(scene_dir,'gt.npy')
+        self.n_depths = len(self.depths_files)
+        self.depth_names = [os.path.basename(f) for f in self.depths_files]
+        
+        self.load_depths()
+        if args.debug:
+            self.debug()
 
-    plt.show()
+    def load_depths(self):
+        # load all depths
+        
+        print('Loading depths for scene: ', self.scene_dir)
+        self.gt = np.load(self.gt_depth_file)
+        self.gt = self.gt[:,:,:,0]
+        for f in self.depths_files:
+            # load depth
+            depth = np.load(f)
+            # scale depth
+            depth *= (1.0/args.scale)
+            
+            self.depths.append(Depth(depth,self.gt))
+
+    def debug(self):
+        ax1 = plt.subplot(1,self.n_depths+1,1)
+        ax1.imshow(self.gt[0])
+        #title first plot 
+        ax1.set_title('Ground Truth')
+
+        for i in range(1,self.n_depths+1):
+            print(i)
+            ax = plt.subplot(1,self.n_depths+1,i+1)
+            ax.imshow(self.depths[i-1].depth[0])
+            ax.set_title(self.depth_names[i-1])
+        
 
 
-# compute error metrics
+        plt.show()
 
-# RMSE
-# N x H x W
+# find all dirs in args.dir
 
-# flatten to (N*H*W)
-gt_depth = gt_depth.flatten()
-infer_depth = infer_depth.flatten()
+scenes = glob.glob(os.path.join(args.dir,'*'))
+# filter out non dirs
+scenes = [s for s in scenes if os.path.isdir(s)]
 
-# remove values off the table
-mask = gt_depth < args.thresh 
-gt_depth = gt_depth[mask]
-infer_depth = infer_depth[mask]
+scenes = [Scene(s) for s in scenes]
 
-rmse = np.sqrt(np.mean((gt_depth - infer_depth)**2))
 
-print('RMSE: ', rmse)
+# # N x H x W
+# gt_depth = np.load(args.gt)[:,:,:,0]
+# # gt_depth = gt_depth[:,:,:,0]
+# # N x H x W
+# infer_depth = np.load(args.infer)
+# infer_depth *= (1.0/args.scale)
+# if args.debug:
+#     ax1 = plt.subplot(1,2,1)
+#     ax1.imshow(gt_depth[0])
+#     ax2 = plt.subplot(1,2,2)
+#     ax2.imshow(infer_depth[0])
+
+#     #title first plot 
+#     ax1.set_title('Ground Truth')
+#     ax2.set_title('Inferred Depth')
+
+#     plt.show()
+
+
+# # compute error metrics
+
+# # RMSE
+# # N x H x W
+
+# # flatten to (N*H*W)
+# gt_depth = gt_depth.flatten()
+# infer_depth = infer_depth.flatten()
+
+# # remove values off the table
+# mask = gt_depth < args.thresh 
+# gt_depth = gt_depth[mask]
+# infer_depth = infer_depth[mask]
+
+# rmse = np.sqrt(np.mean((gt_depth - infer_depth)**2))
+
+# print('RMSE: ', rmse)
